106669e6c7d950fa9793ef1667e9d81f0bfe4768
Marco Morais <marcoemorais@yahoo.com>
2021-07-17 18:11:11 -0700

    Add push_front

--- 04-deque/deque.cc
+++ 04-deque/deque.cc
@@ -13,6 +13,7 @@ struct deque_empty_error : std::runtime_error
     using std::runtime_error::runtime_error;
 };
 
+// node is a node in a double linked list.
 template <typename T>
 struct node
 {
@@ -31,11 +32,10 @@ class Deque
 public:
     Deque() = default;
 
-    // push appends data to end of deque.
-    void push(const T& data)
+    // push_back appends data to end of deque.
+    void push_back(const T& data)
     {
         if (!head) {
-            // Deque is empty.
             head = std::make_unique<node<T>>(data);
             tail = head.get();
         }
@@ -70,6 +70,21 @@ public:
             tail = tail->prev;
         }
         --nelem;
+    }
+
+    // push_front prepends data to the front of the deque.
+    void push_front(const T& data)
+    {
+        if (!head) {
+            head = std::make_unique<node<T>>(data);
+            tail = head.get();
+        }
+        else {
+            auto tmp = std::make_unique<node<T>>(data);
+            tmp->next = std::move(head);
+            head = std::move(tmp);
+        }
+        ++nelem;
     }
 
     // front returns but does not remove from front of deque.
@@ -121,43 +136,59 @@ TEST_CASE("[Deque]")
     REQUIRE_THROWS_AS(deque.pop_back(), deque_empty_error);
     REQUIRE_THROWS_AS(deque.pop_front(), deque_empty_error);
 
-    deque.push(1);
-    REQUIRE(deque.back() == 1);
+    deque.push_front(1); // 1
     REQUIRE(deque.front() == 1);
+    REQUIRE(deque.back() == 1);
     REQUIRE(deque.size() == 1);
 
-    deque.push(2);
-    REQUIRE(deque.back() == 2);
-    REQUIRE(deque.front() == 1);
+    deque.push_front(2); // 2 1
+    REQUIRE(deque.front() == 2);
+    REQUIRE(deque.back() == 1);
     REQUIRE(deque.size() == 2);
 
-    deque.push(3);
+    deque.push_back(3); // 2 1 3
+    REQUIRE(deque.front() == 2);
     REQUIRE(deque.back() == 3);
-    REQUIRE(deque.front() == 1);
     REQUIRE(deque.size() == 3);
 
-    deque.push(4);
+    deque.push_back(4); // 2 1 3 4
+    REQUIRE(deque.front() == 2);
     REQUIRE(deque.back() == 4);
-    REQUIRE(deque.front() == 1);
     REQUIRE(deque.size() == 4);
 
-    deque.pop_front();
+    deque.pop_front(); // 1 3 4
+    REQUIRE(deque.front() == 1);
     REQUIRE(deque.back() == 4);
-    REQUIRE(deque.front() == 2);
     REQUIRE(deque.size() == 3);
 
-    deque.pop_back();
+    deque.pop_back(); // 1 3
+    REQUIRE(deque.front() == 1);
     REQUIRE(deque.back() == 3);
-    REQUIRE(deque.front() == 2);
     REQUIRE(deque.size() == 2);
 
-    deque.pop_back();
-    REQUIRE(deque.back() == 2);
-    REQUIRE(deque.front() == 2);
+    deque.pop_front(); // 3
+    REQUIRE(deque.front() == 3);
+    REQUIRE(deque.back() == 3);
     REQUIRE(deque.size() == 1);
 
-    deque.pop_back();
+    deque.pop_back(); // nil
     REQUIRE(deque.size() == 0);
     REQUIRE_THROWS_AS(deque.pop_back(), deque_empty_error);
     REQUIRE_THROWS_AS(deque.pop_front(), deque_empty_error);
+
+    deque.push_front(1); // 1
+    REQUIRE(deque.front() == 1);
+    REQUIRE(deque.back() == 1);
+    REQUIRE(deque.size() == 1);
+
+    deque.pop_back(); // nil
+    REQUIRE(deque.size() == 0);
+
+    deque.push_back(2); // 2
+    REQUIRE(deque.front() == 2);
+    REQUIRE(deque.back() == 2);
+    REQUIRE(deque.size() == 1);
+
+    deque.pop_back(); // nil
+    REQUIRE(deque.size() == 0);
 }
